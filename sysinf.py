e37aa85d5bbb95393552f0d7f75d9c0a31fe5e12
Rex Fitzhugh <rexreedfitzhugh@gmail.com>
2018-03-20 12:49:08 -0500

    added example output

--- sysinf.py
+++ sysinf.py
@@ -4,6 +4,23 @@ import sys
 import socket
 import os
 
+"""
+EXAMPLE OUTPUT:
+
+Model         K56CA
+Runtime       0d 1h 5m 42s
+CPU Model     Intel(R) Core(TM) i5-3317U CPU @ 1.70GHz
+Sockets       1
+Real Cores    2
+Total Cores   4
+Total Memory  8050780 (7.68gb)
+IP            10.0.0.41
+Gateway       10.0.0.1
+Interface     wlp2s0
+Hostname      rxlx
+Kernel        4.15.9-200.fc26.x86_64
+"""
+
 def get_model():
     mdl = os.popen('sudo dmidecode | grep "Product Name"')
     model = mdl.readline().split(': ')
