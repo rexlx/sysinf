a5347d5f63eb0610eac1c4e4f47865b1ab4dcfdc
Rex Fitzhugh <rexreedfitzhugh@gmail.com>
2018-12-04 18:22:45 -0600

    modified for platform deprecation
    platform deprecated a distro feature, adjusted accordingly.
    
    added the -v option to get the model. will require appropriate permissions to use.

--- sysinf.py
+++ sysinf.py
@@ -1,38 +1,50 @@
 from __future__ import print_function
 import multiprocessing as mp
-import sys, os, socket
+import os, platform, sys, socket
 
-"""
-EXAMPLE OUTPUT:
+def get_args():
+    """
+    gets arguments
+    """
+    # if user adds -v arg
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-v':
+            # they want the model. needs root priv
+            verbose = True
+        return verbose 
+    else:
+        verbose = False
+        return verbose
 
-Model         K56CA
-Runtime       0d 1h 5m 42s
-CPU Model     Intel(R) Core(TM) i5-3317U CPU @ 1.70GHz
-Sockets       1
-Real Cores    2
-Total Cores   4
-Total Memory  8050780 (7.68gb)
-IP            10.0.0.41
-Gateway       10.0.0.1
-Interface     wlp2s0
-Hostname      rxlx
-Kernel        4.15.9-200.fc26.x86_64
-"""
+def get_dist():
+    # this will not work on windows, assume linux
+    dist = platform.platform()
+    #x = ' '.join(e for e in dist)
+    print("Distribution".ljust(14) + dist)
 
 def get_model():
     """
-    this function requires root for dmidecode
+    this gets the model of the machine, requires superuser
+    permissions. it is not on by default, and is invoked
+    with the the -v option
     """
-    mdl = os.popen('sudo dmidecode | grep "Product Name"')
-    model = mdl.readline().split(': ')
-    product = model[1]
-    #return product
-    print('Model'.ljust(14) + product, end='')
+    try:
+        # this leverages dmidecode command
+        mdl = os.popen('dmidecode | grep "Product Name"')
+        model = mdl.readline().split(': ')
+        product = model[1]
+        #return product
+        print('Model'.ljust(14) + product, end='')
+    # exception should be premission denied, it is
+    # possible perhaps ddmidecode command isnt
+    # installed as well.
+    except Exception as e:
+        print("couldn't get_model(), you need higher permissions\n")
 
 def seconds_to_readable(seconds):
     """
-    A python program that converts seconds to
-    human readable times
+    this function converts seconds to human readable times
+    im sure theres a better way, but it works.
     """
     #Copies the number
     s = int(float(seconds))
@@ -49,6 +61,7 @@ def seconds_to_readable(seconds):
         if s < 0:
             # inform user of error
             print("thats less then zero...")
+            error = 'true'
             break
         # if the value is greater or equal to a day in seconds
         elif s >= 86400:
@@ -57,64 +70,64 @@ def seconds_to_readable(seconds):
             day += 1
             # repeat from the top
             continue
-        # if the value is greater or equal to an hour in seconds
+        # if the value
         elif s >= 3600:
-            # subtract an hour from the value and increase the hour count by one
             s -= 3600
             hour += 1
             continue
-        # subtracts minutes and increases the minute counter
         elif s >= 60:
             s -= 60
             mins += 1
             continue
-        # otherwise, the value had been reduced to its final form
         elif s < 60:
-            #stop evaluating the value now
             break
-    # join the the counters from above and return them
     converted_time = str(day) + 'd ' + str(hour) + 'h ' \
                    + str(mins) + 'm ' + str(s) + 's'
     return converted_time
 
 def get_uptime():
-    # open (and close) the uptime file
+    """
+    this function opens the uptime file and, using the
+    seconds_to_readable() function, converts it to
+    something more readable
+    """
     with open('/proc/uptime', 'r') as f:
         for i in f:
-            # splits the line for just the runtime
             line = str(i).split()
             uptime = line[0]
-    # converts that data into human readable times
+    # converted here
     runtime = seconds_to_readable(uptime)
     print('Runtime'.ljust(14) + runtime)
 
 def get_cpu():
-    # open and close the cpu file
+    """
+    this function opens the cpuinfo file and gets some
+    info about the cpu :)
+    """
     with open('/proc/cpuinfo') as f:
-        #using the multiprocessing module, count cores
+        # count the cpu cores with multiprocessing module
+        # includes hyperthreaded cores
         total_cores = mp.cpu_count()
-        # empty lists to append later
+        # make some empty lists for later
         sock_count = []
         core_count = []
-        # for each line in the cpu file
         for line in f:
-            # if (physical id) is in the line
+            # matches physical id
             if 'cal id' in line:
-                # append the socket list
                 sock_count.append(line)
+            # this will get the cpu name
             if 'model name' in line:
-                # split the line for the stuff we need
                 mod = line.split()
                 model = mod[3:]
-                # join into a single string
                 cpu_model = ' '.join(str(e) for e in model)
+            # only counts real cores, not hyper threaded ones
             if 'cpu cores' in line:
                 cores = line.split()
                 core_count.append(cores[3])
-    # convert this list into a set to leave only unique values
+    # convert to set to remove duplicates
     sockets = set(sock_count)
-    # use the last value in the list and multiply it
-    # by the amount of sockets giving you the total real cpus
+    # the amount of total real cores is the real core count
+    # multiplied by the amount of sockets (cpus) on board
     real_cores = int(core_count[-1]) * int(len(sockets))
     print('CPU Model'.ljust(14) + cpu_model)
     print('Sockets'.ljust(14) + str(len(sockets)))
@@ -122,61 +135,81 @@ def get_cpu():
     print('Total Cores'.ljust(14) + str(total_cores))
 
 def get_mem():
-    # open and close meminfo (meminfo values print as kB but are KB
-    # aka KiB
+    """
+    opens meminfo, gets meminfo
+    """
     with open('/proc/meminfo') as f:
         for line in f:
+            # gets total memory
             if 'MemTotal' in line:
                 total = line.split()
                 mem_in_kb = total[1]
-                # convert KB to GB
+                # convert to GiB
                 kb_to_gb = int(mem_in_kb) / (1024 ** 2.0)
-                # get rid of some zeroes
+                # we dont need many decimals, two will do
                 mem_in_gb = "{0:.2f}".format(kb_to_gb)
-    # dazzle
     print('Total Memory'.ljust(14) + mem_in_kb + ' (' + str(mem_in_gb) \
           + 'gb)')
 
 def get_net():
-    # get hostname
+    """
+    get details about network state
+    """
+    # get the hostname via socket module
     hostname = socket.gethostname()
-    # this tests where ip is, since CentOS versions cant agree :)
+    # ip command can be in different places, test for this
     if os.path.isfile('/usr/sbin/ip'):
+        # run ip route, gives us good data
         f = os.popen('/usr/sbin/ip route')
         is_linux_with_ip = True
     elif os.path.isfile('/sbin/ip'):
         f = os.popen('/sbin/ip route')
         is_linux_with_ip = True
     else:
-        # otherwise the ip command is not installed
+        # if no ip, its time to update your server
         print("Missing 'ip' package")
         is_linux_with_ip = False
     if is_linux_with_ip:
         for line in f:
-            #get basic net info
             if 'default' in line:
                 data = line.split()
+                # get default gateway and interface
                 gateway, dev = data[2], data[4]
-    # get ip address
+    # get ip as socket sees it
     ip = socket.gethostbyname(hostname)
     print('IP'.ljust(14) + ip)
     print('Gateway'.ljust(14) + gateway)
     print('Interface'.ljust(14) + dev)
     print('Hostname'.ljust(14) + hostname)
 
+# deprecated since platform deprecated the distro feature
+# will keep for now
 def get_kernel():
+    """
+    get the kernel via uname cmd
+    """
     f = os.popen('uname -r')
     kernel = f.readline()
     print('Kernel'.ljust(14) + kernel)
 
 def main():
-    #get model off by default
+    """
+    this is it, what we've all been waiting for
+    """
+    print('\n')
+    # determine verbisity level from get_args
+    verbose = get_args()
+    if verbose:
+        get_model()
+    # leaving this commented in case someone
+    # wants it on by defualt
     #get_model()
+    get_dist()
     get_uptime()
     get_cpu()
     get_mem()
     get_net()
-    get_kernel()
+    #get_kernel()
 
 if __name__ == '__main__':
     main()
